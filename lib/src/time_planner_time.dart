7034f7602c440c0760d7f23e196958adb8f3066f
SayaliMoghe <164007923+SayaliMoghe@users.noreply.github.com>
2024-08-10 10:29:42 +0530

    Update file for Capriole

--- lib/src/time_planner_time.dart
+++ lib/src/time_planner_time.dart
@@ -1,67 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:time_planner/src/config/global_config.dart' as config;
 
-/// Title widget for time planner
-class TimePlannerTitle extends StatelessWidget {
-  /// Title of each day, typically is name of the day for example sunday
-  ///
-  /// but you can set any things here
-  final String title;
+/// Show the hour for each row of time planner
+class TimePlannerTime extends StatelessWidget {
+  /// Text it will be show as hour
+  final String? time;
+  final bool? setTimeOnAxis;
 
-  /// Text style for title
-  final TextStyle? titleStyle;
-
-  /// Date of each day like 03/21/2021 but you can leave it empty or write other things
-  final String? date;
-
-  /// Text style for date text
-  final TextStyle? dateStyle;
-final double? width;
-final double? height;
-
-  /// Title widget for time planner
-  const TimePlannerTitle({
+  /// Show the hour for each row of time planner
+  const TimePlannerTime({
     Key? key,
-    required this.title,
-    this.date,
-    this.titleStyle,
-    this.dateStyle,
-    this.width,
-    this.height
+    this.time,
+    this.setTimeOnAxis,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: height,
-        width:width,
+      height: config.cellHeight!.toDouble(),
+      width: config.cellWidth!*.84,
       decoration: BoxDecoration(
-        border: Border(left: BorderSide(color: Colors.white, width: 1.0),bottom: BorderSide(color: Colors.white, width: 1.0)),
-        color: Color.fromRGBO(109, 187, 255, 1)
+        border: Border(top: BorderSide(color: Colors.white, width: 1.0),right: BorderSide(color: Colors.white, width: 1.0)),
+          color: Color.fromRGBO(109, 187, 255, 1),
       ),
-      child: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              title,
-              style: titleStyle ?? const TextStyle(fontWeight: FontWeight.w600),
-            ),
-            const SizedBox(
-              height: 2,
-            ),
-            date!=null
-            ?Text(
-              date ?? '',
-              style: dateStyle ??
-                  const TextStyle(color: Colors.white, fontSize: 12),
-            )
-            :SizedBox(),
-
-          ],
-        ),
-      ),
+      child: setTimeOnAxis! ? Text(time!,style: TextStyle(fontWeight: FontWeight.w400, fontFamily: 'Poppins', fontSize: 12,color: Color.fromRGBO(255, 255, 255, 1)),) : Center(child: Text(time!,style: TextStyle(fontWeight: FontWeight.w400, fontFamily: 'Poppins', fontSize: 12,color: Color.fromRGBO(255, 255, 255, 1)),)),
     );
   }
 }
